86c7777682675c074ea99473b0619a4a1c6a0a78
huntergera <huntergera91@gmail.com>
2020-11-15 22:29:01 +0200

    fixed bug

--- src/components/modal-form/modal-form.js
+++ src/components/modal-form/modal-form.js
@@ -18,20 +18,20 @@ class ModalForm {
     }
 
     setInputsValue() {
-        this.form.querySelector("#title").value = this.movie.title;
-        this.form.querySelector("#titleOriginal").value = this.movie.titleOriginal;
-        this.form.querySelector("#image").value = this.movie.image;
-        this.form.querySelector("#year").value = this.movie.year;
-        this.form.querySelector("#country").value = this.movie.country;
-        this.form.querySelector("#slogan").value = this.movie.slogan;
-        this.form.querySelector("#director").value = this.movie.director;
-        this.form.querySelector("#producer").value = this.movie.producer;
-        this.form.querySelector("#scenario").value = this.movie.scenario;
-        this.form.querySelector("#roles").value = this.movie.roles;
-        this.form.querySelector("#operator").value = this.movie.operator;
-        this.form.querySelector("#composer").value = this.movie.composer;
-        this.form.querySelector("#rating").value = this.movie.rating;
-        this.form.querySelector("#text").value = this.movie.text;
+        this.form.querySelector("#title").value = this.movie.title || "";
+        this.form.querySelector("#titleOriginal").value = this.movie.titleOriginal || "";
+        this.form.querySelector("#image").value = this.movie.image || "";
+        this.form.querySelector("#year").value = this.movie.year || "";
+        this.form.querySelector("#country").value = this.movie.country || "";
+        this.form.querySelector("#slogan").value = this.movie.slogan || "";
+        this.form.querySelector("#director").value = this.movie.director || "";
+        this.form.querySelector("#producer").value = this.movie.producer || "";
+        this.form.querySelector("#scenario").value = this.movie.scenario || "";
+        this.form.querySelector("#roles").value = this.movie.roles || "";
+        this.form.querySelector("#operator").value = this.movie.operator || "";
+        this.form.querySelector("#composer").value = this.movie.composer || "";
+        this.form.querySelector("#rating").value = this.movie.rating || "";
+        this.form.querySelector("#text").value = this.movie.text || "";
     }
 
     render() {
